dd8ac5c7cea46f974bb0a188d2a4e7c002f7f904
TXJK <javadsalehi1994@gmail.com>
2022-01-29 11:28:26 +0330

    add script for hide menu after click on the links

--- edie-homepage/assets/js/script.js
+++ edie-homepage/assets/js/script.js
@@ -16,3 +16,12 @@ if(navClose) {
     });
 }
 
+
+/*********************** hide menu after click on the link *********************************/
+
+const links = document.querySelectorAll("#links");
+links.forEach((item) => {
+    item.addEventListener("click", () => {
+        navModal.classList.remove("show__menu");
+    })
+});
